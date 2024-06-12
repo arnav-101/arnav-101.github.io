99907e802c74c912459697b58158135015fa2410
arnav-101 <assassinhypertext190@gmail.com>
2024-06-12 11:54:31 +0530

    Updated Nav-Bar 'added active class'

--- app.js
+++ app.js
@@ -5,3 +5,16 @@ hamBurger.addEventListener('click', () => {
     hamBurger.classList.toggle('active');
     hamMenu.classList.toggle('active');
 })
+
+
+const navbarList = document.querySelectorAll(".list-item");
+
+navbarList.forEach(item => {
+    item.addEventListener('click', function() {
+        // Remove active class from all items
+        navbarList.forEach(navItem => navItem.classList.remove('active'));
+        console.log("clicked");
+        // Add active class to the clicked item
+        this.classList.add('active');
+    });
+});
