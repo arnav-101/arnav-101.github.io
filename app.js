97b6a9bf02c6fdf19eeb0c448db77b47f1ec77f6
arnav-101 <assassinhypertext190@gmail.com>
2024-06-13 19:23:26 +0530

    Added Scroll Animation

--- app.js
+++ app.js
@@ -19,3 +19,37 @@ navbarList.forEach(item => {
     });
 });
 
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+
+const hiddenElements = document.querySelectorAll('.hidden-animation');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
