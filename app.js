3736c311ce36b6ff62ee8fd74020e327497ef781
arnav-101 <assassinhypertext190@gmail.com>
2024-06-13 11:34:24 +0530

    Home and About me Done

--- app.js
+++ app.js
@@ -19,35 +19,3 @@ navbarList.forEach(item => {
     });
 });
 
-const texts = ["front-end developer", "an 11th grader", "an enthusiastic learner"];
-let index = 0;
-let charIndex = 0;
-const typingSpeed = 100;
-const erasingSpeed = 50;
-const delayBetweenTexts = 2000;
-const dynamicText = document.getElementById("dynamicText");
-
-function typeText() {
-  if (charIndex < texts[index].length) {
-    dynamicText.textContent += texts[index].charAt(charIndex);
-    charIndex++;
-    setTimeout(typeText, typingSpeed);
-  } else {
-    setTimeout(eraseText, delayBetweenTexts);
-  }
-}
-
-function eraseText() {
-  if (charIndex > 0) {
-    dynamicText.textContent = texts[index].substring(0, charIndex - 1);
-    charIndex--;
-    setTimeout(eraseText, erasingSpeed);
-  } else {
-    index = (index + 1) % texts.length;
-    setTimeout(typeText, typingSpeed);
-  }
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  setTimeout(typeText, delayBetweenTexts);
-});
